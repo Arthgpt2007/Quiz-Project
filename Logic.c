e4fbfb4681b536eee65ac13b1624f8d812049937
Arth Gupta <Arth.23014@stu.upes.ac.in>
2025-11-28 21:25:39 +0530

    Adding

--- Logic.c
+++ Logic.c
@@ -10,6 +10,44 @@ struct Question
     int correctOption;
 };
 
+//All the rules of the game
+void GiveInstructions()
+{
+    printf("1) There will be a total of 10 questions. \n");
+    printf("2) Every 3RD question will be a BONUS question(More difficult but will both reward and deduce more points) \n");
+    printf("3) Every CORRECT answer to a NORMAL question will award 5 points \n");
+    printf("4) Every CORRECT answer to a BONUS question will award 10 points \n");
+    printf("5) Every INCORRECT answer to a NORMAL question will deduce 2 points \n");
+    printf("6) Every INCORRECT answer to a BONUS question will deduce 4 points \n");
+    printf("All the Best and have fun! \n");
+}
+
+//Start the game control
+void StartGame()
+{
+    //Printing The option for either rules or to start the game
+    printf("Welcome to The QUIZ GAME made my ARTH GUPTA AND TANISH BHADOLA \n");
+    printf("Enter 1 For Rules \n");
+    printf("Enter 2 to start the game! \n");
+
+    int choice; //Variable to store choice by the user
+    scanf("%d", &choice);
+    
+    //Switch case for either displaying the instructions(GiveInstruction() function) or start the game
+    switch(choice)
+    {
+        case 1:
+            GiveInstructions();
+        break;
+        case 2:
+            printf("Best Of Luck!( You will need it ;-) ) \n");
+        break;
+        default:
+            printf("Wrong Choice! Exiting \n");
+        break;
+    }
+}
+
 //Function to add score for each correct answer
 void AssignScore(struct Question *qn)
 {
@@ -55,13 +93,26 @@ void ScoreSystem(struct Question *qn, int currentScore)
     }
 }
 
-void MultiplierSystem() //Thinking about it ATM
+//Thinking about it ATM
+void MultiplierSystem() 
 {
 
 }
 
+//Thinking About Implementing It
+void RandomBonusGenerator()
+{
+
+}
+
+//Main Function
 int main()
 {
+
+    //Display the instructions of the game
+    StartGame();
+
+
     //Initializing 10 different question structures
     struct Question que1;
     struct Question que2;   
@@ -72,10 +123,7 @@ int main()
     struct Question que7;   
     struct Question que8;   
     struct Question que9;   
-    struct Question que10;   
-
-    //Initilaizing initial scors of all structures as 0
-    
+    struct Question que10;       
 
     //Assigning every 3rd question as the bonus question
     que3.isBonusQn = true;
@@ -105,9 +153,7 @@ int main()
 
 
 
-    //Debugging is bonux question if of 10 marks
-    printf("%d", que1.score);
-    printf("%d", que3.score);
+    
 
     
 
